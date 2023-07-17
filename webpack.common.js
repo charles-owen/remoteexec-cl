0d9c7f97bf67e6f9f1239bca14c7d8762910e9d8
Dr. Charles B. Owen <cbowen@msu.edu>
2023-07-17 10:23:04 -0400

    Updated to new build method

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 module.exports = {
 	entry: {
-		RemoteExec: path.resolve(__dirname, 'index.js')
+    RemoteExec: {
+      import: path.resolve(__dirname, 'index.js'),
+      dependOn: ['Course', 'Users', 'Site']
+    }
 	}
 }
